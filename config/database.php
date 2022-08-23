c3e3f253cbf0b6dea671be31238d25d4aa41f35c
Hans183 <hvonedelsberg@gmail.com>
2022-08-23 11:45:46 -0400

    database original

--- config/database.php
+++ config/database.php
@@ -45,15 +45,6 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-<<<<<<< HEAD
-            'url' => 'DATABASE_URL',
-            'host' => 'DB_HOST', 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-            'port' => 'DB_PORT', '3306',
-            'database' => 'DB_DATABASE', 'kdu33tyr3ogoo6h0',
-            'username' => 'DB_USERNAME', 'iz2v1bigp5ekf0xs',
-            'password' => 'DB_PASSWORD', 'tpklcxurpx2q5905',
-            'unix_socket' => 'DB_SOCKET', '',
-=======
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -61,7 +52,6 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
->>>>>>> parent of a6c8750 (Modifico database, porque ignora env)
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
