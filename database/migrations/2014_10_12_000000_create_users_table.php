437865abf89b36cba08740529172392b9a5f3824
Hans183 <hvonedelsberg@gmail.com>
2022-08-23 18:42:18 -0400

    Ahora PAssword reset

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('apellido');
-            $table->string('rut',15)->unique();
+            $table->string('rut',100)->unique();
             $table->string('email',100)->unique();
             $table->date('fecha_nacimiento');
             $table->timestamp('email_verified_at')->nullable();
