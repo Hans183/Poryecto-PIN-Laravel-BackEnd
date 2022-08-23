e93c67276714be1568673296a667f83b51a3fd28
Hans183 <hvonedelsberg@gmail.com>
2022-08-23 19:32:08 -0400

    string a text cmessage

--- database/migrations/2022_06_24_205852_create_contactos_table.php
+++ database/migrations/2022_06_24_205852_create_contactos_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone');
-            $table->string('cmessage');
+            $table->text('cmessage');
             $table->id();
             $table->timestamps();
             $table->softDeletes();
